bfa3ba7e008745cca533048629905306bc9f46cc
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-03-05 22:43:06 +0100

    Monorepo with tsconfig paths and stuff

--- jest.config.js
+++ jest.config.js
@@ -20,3 +20,65 @@ module.exports = {
   ],
   modulePathIgnorePatterns: ['<rootDir>/dist'],
 };
+
+// import { dirname, extname } from 'path';
+// import * as ts from 'typescript';
+// import defaultResolver from 'jest-resolve/build/defaultResolver';
+
+// interface ResolveOptions {
+//   rootDir: string;
+//   basedir: string;
+//   paths: string[];
+//   moduleDirectory: string[];
+//   browser: boolean;
+//   extensions: string[];
+//   defaultResolver: typeof defaultResolver;
+// }
+
+// function getCompilerSetup(rootDir: string) {
+//   const tsConfigPath =
+//     ts.findConfigFile(rootDir, ts.sys.fileExists, 'tsconfig.spec.json') ||
+//     ts.findConfigFile(rootDir, ts.sys.fileExists, 'tsconfig.test.json') ||
+//     ts.findConfigFile(rootDir, ts.sys.fileExists, 'tsconfig.jest.json');
+
+//   if (!tsConfigPath) {
+//     console.error(
+//       `Cannot locate a tsconfig.spec.json. Please create one at ${rootDir}/tsconfig.spec.json`
+//     );
+//   }
+
+//   const readResult = ts.readConfigFile(tsConfigPath, ts.sys.readFile);
+//   const config = ts.parseJsonConfigFileContent(
+//     readResult.config,
+//     ts.sys,
+//     dirname(tsConfigPath)
+//   );
+//   const compilerOptions = config.options;
+//   const host = ts.createCompilerHost(compilerOptions, true);
+//   return { compilerOptions, host };
+// }
+
+// let compilerSetup;
+
+// module.exports = function(path: string, options: ResolveOptions) {
+//   const ext = extname(path);
+//   if (
+//     ext === '.css' ||
+//     ext === '.scss' ||
+//     ext === '.sass' ||
+//     ext === '.less' ||
+//     ext === '.styl'
+//   ) {
+//     return require.resolve('identity-obj-proxy');
+//   }
+//   // Try to use the defaultResolver
+//   try {
+//     return defaultResolver(path, options);
+//   } catch (e) {
+//     // Fallback to using typescript
+//     compilerSetup = compilerSetup || getCompilerSetup(options.rootDir);
+//     const { compilerOptions, host } = compilerSetup;
+//     return ts.resolveModuleName(path, options.basedir, compilerOptions, host)
+//       .resolvedModule.resolvedFileName;
+//   }
+// };
